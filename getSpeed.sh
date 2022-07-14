cbec4ce195979e49644d4c86e79bfab492b34386
raghav <raghavdonakanti@gmail.com>
2022-07-14 17:33:18 +0530

    Fixed issue of script running in another directory with cron

--- getSpeed.sh
+++ getSpeed.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
 
 
-echo "Checking speed..."
+#echo "Checking speed..."
+#gets the stats in space separated format
 stats=$(speedtest --csv-delimiter " " --csv)
 
 
 #YYYY-MM-DDThh:mm
 time=$(date +%y%m%d%H%M)
-echo "time is $time"
+#echo "time is $time"
 
 # Mbits/s
 stats=($stats)
 download=$(echo "scale=4;${stats[6]}/1000000" | bc)
 upload=$(echo "scale=4;${stats[7]}/1000000" | bc)
 
-echo "download : $download"
-echo "upload : $upload"
+#echo "download : $download"
+#echo "upload : $upload"
 
+#echo $0
+# gets the path where the database needs to be created(usually the path of this script)
+path=$(echo $0 | awk 'BEGIN {FS="/"};{for(i=1;i<=(NF-1);i++) {printf("%s/",$i)}};')
+#echo $path
 
-cd .
+# since its running on cron, it runs this script from a separate place
+cd $path
 
 tables=$(echo "select name from sqlite_master where type='table'" | sqlite3 test1.db)
 
+# checking if table exists
 if [[ $tables != "data" ]]
 then
 echo "create table data (time varchar(100),download float,upload float)" | sqlite3 test1.db
