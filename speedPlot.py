a3899e3ccc79a12f59f2031c8254ad28a1638293
raghav <raghavdonakanti@gmail.com>
2022-07-17 18:22:46 +0530

    changed the graph layout and style

--- speedPlot.py
+++ speedPlot.py
@@ -5,32 +5,34 @@ import matplotlib.pyplot as plt
 db=dbm.connect("test1.db")
 dbc=db.cursor()
 
+# gets todays date
 today=str(datetime.now().strftime('%y%m%d'))
 
 dbc.execute('select * from data where time like (?)',[today+'%'])
 
 todaysData=dbc.fetchall()
 
+dbc.close()
+db.close()
+
 timestamps=[]
 download=[]
 upload=[]
 
 for i in todaysData:
+    # timestamp in YYMMDDhhmm format
     timestamps.append(i[0][6:8]+':'+i[0][8:])
     download.append(float(i[1]))
     upload.append(float(i[2]))
 
-fig,axes=plt.subplots(1,2,sharey=True)
+fig,ax=plt.subplots()
 
-axes[0].plot(timestamps,download)
-axes[0].set_title('Download')
-axes[0].set_xlabel('Timestamps')
-axes[0].set_ylabel('Download Speed')
-
-axes[1].plot(timestamps,upload)
-axes[1].set_title('Upload')
-axes[1].set_xlabel('Timestamps')
-axes[1].set_ylabel('Upload Speed')
+ax.plot(timestamps,download,color='blue',label='Download')
+ax.plot(timestamps,upload,color='red',linestyle='dashed',label='Upload')
+ax.set_xlabel('Timestamp')
+ax.set_ylabel('(in Mbit/s)')
+ax.set_title('Speeds')
+ax.legend()
 
 plt.show()
 
