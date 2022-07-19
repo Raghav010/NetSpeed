f31f86b38a25465d34444544c2f6744fc5542d95
raghav <raghavdonakanti@gmail.com>
2022-07-19 20:00:54 +0530

    implemented a cleaner file organization structure and modified code to reflect this

---
+++ .soft/speedPlot.py
@@ -0,0 +1,38 @@
+import sqlite3 as dbm
+from datetime import datetime
+import matplotlib.pyplot as plt 
+
+db=dbm.connect(".data/speeds.db")
+dbc=db.cursor()
+
+# gets todays date
+today=str(datetime.now().strftime('%y%m%d'))
+
+dbc.execute('select * from data where time like (?)',[today+'%'])
+
+todaysData=dbc.fetchall()
+
+dbc.close()
+db.close()
+
+timestamps=[]
+download=[]
+upload=[]
+
+for i in todaysData:
+    # timestamp in YYMMDDhhmm format
+    timestamps.append(i[0][6:8]+':'+i[0][8:])
+    download.append(float(i[1]))
+    upload.append(float(i[2]))
+
+fig,ax=plt.subplots()
+
+ax.plot(timestamps,download,color='blue',label='Download')
+ax.plot(timestamps,upload,color='red',linestyle='dashed',label='Upload')
+ax.set_xlabel('Timestamp')
+ax.set_ylabel('(in Mbit/s)')
+ax.set_title('Speeds')
+ax.legend()
+
+plt.show()
+
--- install.sh
+++ install.sh
@@ -12,18 +12,19 @@ absolutePath="${0/"./"/"$runningDir/"}"
 
 # gets the absolute directory for this script
 # so that the script can be run from any location
-absoluteDir=$(echo $absolutePath | awk 'BEGIN {FS="/"};{for(i=1;i<=(NF-1);i++) {printf("%s/",$i)}};')
+absoluteDir=$(echo "$absolutePath" | awk 'BEGIN {FS="/"};{for(i=1;i<=(NF-1);i++) {printf("%s/",$i)}};')
 # echo "absolute directory is $absoluteDir"
 
 
 # going to the application directory
-cd $absoluteDir
+cd "$absoluteDir"
+
 
 # temp holds the new crontab
 sudo crontab -l -u root > temp
 
 # the cron entry in the crontab for the getSpeed script(every 10 minutes)
-cronEntry="*/10 * * * * "$absoluteDir"getSpeed.sh"
+cronEntry="*/10 * * * * "$absoluteDir".soft/getSpeed.sh"
 # echo "$cronEntry"
 
 # checking if the cron job already exists
@@ -43,11 +44,11 @@ rm temp
 
 #adding the alias to run the plotting software
 
-abash=$(grep -F "alias plotSpeed='cd $absoluteDir && python3 speedPlot.py &'" $HOME/.bashrc)
+abash=$(grep -F "alias plotSpeed='cd $absoluteDir && python3 .soft/speedPlot.py &'" $HOME/.bashrc)
 
 if [[ $abash == "" ]]
 then
-echo "alias plotSpeed='cd $absoluteDir && python3 speedPlot.py &'" >> $HOME/.bashrc
+echo "alias plotSpeed='cd $absoluteDir && python3 .soft/speedPlot.py &'" >> $HOME/.bashrc
 echo "" >> $HOME/.bashrc
 source $HOME/.bashrc
 fi
