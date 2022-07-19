eca3100e5abe3ea91cba23c8dacd37516b0e34cd
raghav <raghavdonakanti@gmail.com>
2022-07-19 23:15:07 +0530

    Cleaned up code, removed debugging code and extraneous comments

--- .soft/getSpeed.sh
+++ .soft/getSpeed.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
 
 
-#echo "Checking speed..."
 #gets the stats in space separated format
 stats=$(speedtest --csv-delimiter " " --csv)
 
 
 #YYYY-MM-DDThh:mm
 time=$(date +%y%m%d%H%M)
-#echo "time is $time"
 
 # Mbits/s
 stats=($stats)
 download=$(echo "scale=4;${stats[6]}/1000000" | bc)
 upload=$(echo "scale=4;${stats[7]}/1000000" | bc)
 
-#echo "download : $download"
-#echo "upload : $upload"
 
-#echo $0
+
 # gets the path where the database needs to be created(usually the path of this script)
 # so that this script can be run from any location
 path=$(echo $0 | awk 'BEGIN {FS="/"};{for(i=1;i<=(NF-2);i++) {printf("%s/",$i)}};')
-#echo $path
+
 
 # since its running on cron, it runs this script from a separate place
 cd "$path"
@@ -31,6 +27,7 @@ cd .data/
 
 tables=$(echo "select name from sqlite_master where type='table'" | sqlite3 speeds.db)
 
+
 # checking if table exists
 if [[ $tables != "data" ]]
 then
@@ -38,8 +35,6 @@ echo "create table data (time varchar(100),download float,upload float)" | sqlit
 fi
 
 echo "insert into data values ('$time',$download,$upload)" | sqlite3 speeds.db
-
-
 
 
 
--- install.sh
+++ install.sh
@@ -2,18 +2,15 @@
 
 # need admin privileges 
 
-# the directory where this script is being run from
 runningDir=$(pwd)
-# echo "pwd is $runningDir"
+
 
 # gets the absolute path of the script(replaces ./ with runningDir)
 absolutePath="${0/"./"/"$runningDir/"}"
-# echo "absolute path is $absolutePath"
 
 # gets the absolute directory for this script
 # so that the script can be run from any location
 absoluteDir=$(echo "$absolutePath" | awk 'BEGIN {FS="/"};{for(i=1;i<=(NF-1);i++) {printf("%s/",$i)}};')
-# echo "absolute directory is $absoluteDir"
 
 
 # going to the application directory
@@ -23,13 +20,11 @@ cd "$absoluteDir"
 # temp holds the new crontab
 sudo crontab -l -u root > temp
 
-# the cron entry in the crontab for the getSpeed script(every 10 minutes)
+
 cronEntry="*/10 * * * * "$absoluteDir".soft/getSpeed.sh"
-# echo "$cronEntry"
 
 # checking if the cron job already exists
 aCronned=$(grep -F "$cronEntry" temp)
-# echo "$aCronned"
 
 if [[ $aCronned == "" ]]
 then
@@ -38,12 +33,10 @@ echo "" >> temp # newline after the last cron entry
 fi
 
 sudo crontab -u root temp
-
 rm temp
 
 
 #adding the alias to run the plotting software
-
 abash=$(grep -F "alias plotSpeed='cd $absoluteDir && python3 .soft/speedPlot.py &'" $HOME/.bashrc)
 
 if [[ $abash == "" ]]
